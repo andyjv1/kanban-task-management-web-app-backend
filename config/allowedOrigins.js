9cfccef920dedf739195266038ff78c86e457833
Andy <111096875+andyjv1@users.noreply.github.com>
2023-07-23 10:53:00 -0400

    update allowed websites

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000','http://localhost:3001',
+    'http://localhost:3000', 'http://localhost:3001', 'https://kanban-task-management-web-app.onrender.com'
 ]
 
 module.exports = allowedOrigins
